2b817c15e6aabecafffc0302e29000a3386de45b
Hidekazu Kobayashi <kobahide789@gmail.com>
2014-09-25 17:11:59 +0900

    impl resource and task

--- index.js
+++ index.js
@@ -1,40 +1,142 @@
 var http = require('http'),
-    util = require('util');
+    url = require('url'),
+    util = require('util'),
+    path = require('path'),
+    pointer = require('./pointer'),
+    patch = require('./patch'),
+    inflect = require('i')();
 
-if (global.hasOwnProperty('DOMParser')) {
-  global.DOMParser = require('xmldom').DOMParser;
+function NOOP () { /* NOOP */ }
+
+if (!global.hasOwnProperty('Promise')) {
+  global.Promise = require('es6-promise').Promise;
 }
 
-function Resource (qlient) {
-  this.qlient = qlient;
-  this.isSuspective = true;
+if (!global.hasOwnProperty('XMLHttpRequest')) {
+  global.XMLHttpRequest = require('xhr2');
 }
 
-Resource.T = function (qlient, name, schema) {
-  function ResourceT() {
-    this.$name = name;
+function AbstractResource (qlient, id) {
+  if (typeof id === 'string') {
+    var instances = this.constructor.instances;
+
+    if (instances.hasOwnProperty(id)) {
+      return instances[id];
+    } else {
+      instances[id] = this;
+    }
   }
-  util.inherits(ResourceT, Resource);
-  return ResourceT;
-};
 
-Resource.prototype._makeRefArrayProp = function () {
-  return function (index) {
-    
-  };
-};
+  this.$_qlient = qlient;
 
-Resource.prototype.$resolve = function () {
-  
-};
-
-function Qlient () {
-  this.models = {};
+  this.$_name = null;
+  this.$_id = null;
+  this.$_isSuspective = true;
+  this.$_value = {};
+  this.$_etag = null;
 }
 
-Qlient.load = function () {
-  // TODO
+AbstractResource.all = function () {
+  return qlient.requestRESTfully('GET', this.$name).then(function (list) {
+    return list.map(function (value) {
+      var res = new this(value.id);
+      res.$sync(value, false);
+      return res;
+    }.bind(this));
+  }.bind(this));
 };
+
+AbstractResource.T = function (qlient, name) {
+  function Resource (id) {
+    AbstractResource.call(this, qlient, id);
+
+    this.$_name = name;
+    this.$_id = id || null;
+  }
+  util.inherits(Resource, AbstractResource);
+
+  Resource.instances = {};
+  Resource.all = AbstractResource.all;
+  Resource.$name = name;
+
+  return Resource;
+};
+
+AbstractResource.prototype.$sync = function (newValue, canDelete) {
+  // TODO: check if value is an object
+  var diff = patch.diff(this.$_value, newValue).filter(function (patch) {
+    return canDelete || patch.op !== 'remove';
+  });
+  patch.patch(this, diff);
+  this.$_value = newValue;
+  if (typeof this.$_id !== 'string') {
+    this.constructor.instances[this.id] = this;
+    this.$_id = this.id;
+  }
+};
+
+AbstractResource.prototype.$resolve = function () {
+  return this.$_qlient.requestRESTfully('GET', this.$_name, this.$_id)
+    .then(function (newValue) {
+      return this.$sync(newValue, true);
+    }.bind(this));
+};
+
+AbstractResource.prototype.$transaction = function (cb, queries) {
+  cb = cb || NOOP;
+  return Promise.resolve(cb(this)).then(function () {
+    var newValue = Object.keys(this).filter(function (key) {
+      return key.indexOf('$') !== 0;
+    }).reduce(function (obj, key) {
+      obj[key] = this[key];
+      return obj;
+    }.bind(this), {});
+
+    (function () {
+      if (this.$_id === null) {
+        return this.$_qlient.requestRESTfully(
+          'POST', this.$_name, null, queries, {}, newValue);
+      } else {
+        return this.$_qlient.requestRESTfully(
+          'PUT', this.$_name, this.$_id, queries, {}, newValue);
+      }
+    }).call(this).then(function (newValue) {
+      return this.$sync(newValue);
+    });
+  }.bind(this));
+};
+
+function AbstractTask () {
+}
+util.inherits(AbstractTask, AbstractResource);
+
+AbstractTask.T = function (qlient, name) {
+  function Task (id) {
+    AbstractTask.call(this, qlient, id);
+
+    this.$_name = name;
+    this.$_id = id || null;
+  }
+  util.inherits(Task, AbstractTask);
+
+  Task.instances = {};
+  Task.all = AbstractResource.call;
+  Task.$name = name;
+  Task.queue = function (procedure, isSync) {
+    isSync = isSync || false;
+    var task = new Task();
+    return task.$transaction(function () {
+      task.procedure = procedure;
+    }, { isSync: isSync });
+  };
+
+  return Task;
+};
+
+function Qlient (baseUrl) {
+  this.models = {};
+  this.baseUrl = baseUrl;
+}
 
 Qlient.prototype.def = function (name, model) {
   return this.models[name] = model;
@@ -44,28 +146,71 @@ Qlient.prototype.model = function (name) {
   return this.models[name];
 };
 
-function Task () {
-  
-}
+Qlient.prototype.res = function (name) {
+  return this.def(name, AbstractResource.T(this, name));
+};
+
+Qlient.prototype.request = function (method, pathname, headers, body) {
+  headers = headers || {};
+  var endPoint = url.resolve(this.baseUrl, pathname);
+  console.log(endPoint);
+  return new Promise(function (resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+      resolve(xhr);
+    };
+    xhr.onerror = function (err) {
+      reject(err);
+    };
+    xhr.open(method, endPoint);
+    Object.keys(headers).forEach(function (key) {
+      xhr.setRequstHeader(key, headers[key]);
+    });
+    xhr.send(body);
+  });
+};
+
+Qlient.prototype.requestRESTfully = function (method, res, id, queries, headers, body) {
+  res = inflect.pluralize(res);
+  id = id || '';
+  queries = queries || {};
+  var urlPath = url.stringify({
+    pathname: path.join([ res, id ]),
+    query: queries
+  });
+  return this.request(method, urlPath, headers, JSON.stringify(body))
+    .then(function (xhr) {
+      return JSON.parse(xhr.response);
+    });
+};
 
 if (!module.parent) {
-  var qlient = Qlient.fromSchema({
-    resources: [
-      {
-        id: 'domain',
-        methods: ['GET', 'POST'],
-        schema: {}
-      }
-    ]
-  });
+  var qlient = new Qlient('http://yuno:6544/edj/v1/');
 
   var Topic = qlient.res('topic');
 
-  var topic = Topic.get('id');
+  //var topic = new Topic('354e16a6-4759-46a5-997e-253cd9a35d9f');
 
-  topic.$transaction();
+  /*
+  topic.$resolve().then(function () {
+    console.log(topic);
+  }).catch(function (err) {
+    console.log(err);
+  });
+   */
 
-  topic.publishers(0).$resolve(function () {
-    
+  var topic = new Topic();
+
+  topic.$transaction(function (topic) {
+    console.log('transaction');
+    topic.description = 'foobar';
+  }).then(function () {
+    console.log(topic);
+  }).catch(function (err) {
+    console.log(err);
+  });
+
+  Topic.all().then(function (list) {
+    console.log(list);
   });
 }
--- schema.js
+++ schema.js
@@ -1,4 +1,7 @@
-var url = require('url');
+var util = require('util'),
+    URI = require('URI-js'),
+    reference = require('./reference'),
+    metaschemaJSON = require('./metaschema');
 
 function _isCircular (stack, obj) {
   if (stack.indexOf(obj) !== -1) {
@@ -26,13 +29,234 @@ function isCircular (obj) {
   return _isCircular([], obj);
 }
 
-function Schema () {
-  this.type = '';
+/*
+ * Ref:
+ * JSON Schema: core definitions and terminology
+ *   7.2.  URI resolution scope alteration with the "id" keyword
+ * 
+ * http://json-schema.org/latest/json-schema-core.html#anchor27
+ */
+function expandScope (scope) {
+  scope = reference.normalizeScopeKeys(scope);
+  
+  function traverse (baseUri, obj) {
+    if (Array.isArray(obj)) {
+      // array
+      return obj.map(traverse.bind(null, baseUri));
+    } else if (typeof obj === 'object' && obj !== null) {
+      // object
+      if (obj.hasOwnProperty('id')) {
+        var key = reference.normalizeRefUri(URI.resolve(baseUri, obj.id));
+        scope[key] = obj;
+        return { '$ref': key };
+      } else {
+        // normal
+        return Object.keys(obj).reduce(function (newObj, key) {
+          newObj[key] = traverse(baseUri, obj[key]);
+          return newObj;
+        }, {});
+      }
+    } else {
+      // primitive
+      return obj;
+    }
+  }
+
+  Object.keys(scope).forEach(function (baseUri) {
+    scope[baseUri] = traverse(baseUri, scope[baseUri]);
+  });
+
+  return scope;
 }
 
-Schema.fromJSONSchema = function fromJSONSchema (jsonSchema) {
-  
+var metaschemaInverseIndex = reference.makeInverseIndex(
+  reference.normalizeScopeKeys(metaschemaJSON));
+
+function Schema (scope, entryPoint) {
+  this.$scope = reference.resolve(expandScope(scope));
+  this.$entryPoint = reference.normalizeRefUri(entryPoint);
+  var schemaObject = this.$scope[this.$entryPoint];
+  /* 
+   * TODO: validate schema object here
+   */
+  function replace (stack, obj, a, b) {
+    if (stack.indexOf(obj) !== -1) { return obj; }
+    if (obj === a) { return b; }
+
+    stack.push(obj);
+    if (Array.isArray(obj)) {
+      // array
+      obj.forEach(function (v, i) {
+        obj[i] = replace(stack, v, a, b);
+      });
+    } else if (typeof obj === 'object' && obj !== null) {
+      // object
+      Object.keys(obj).forEach(function (key) {
+        obj[key] = replace(stack, obj[key], a, b);
+      });
+    }
+    stack.pop(obj);
+    return obj;
+  }
+  Schema.keywords.forEach(function (keyword) {
+    if (schemaObject.hasOwnProperty(keyword)) {
+      this[keyword] = schemaObject[keyword];
+      //replace([], schemaObject[keyword], schemaObject, this);
+    } else if (metaschemaJSON.properties[keyword].hasOwnProperty('default')) {
+      this[keyword] = metaschemaJSON.properties[keyword]['default'];
+    } else {
+      this[keyword] = undefined;
+    }
+  }.bind(this));
+}
+
+Schema.keywords = [
+  'multipleOf', 'maximum', 'exclusiveMaximum', 'minimum', 'exclusiveMinimum',
+
+  'maxLength', 'minLength', 'pattern',
+
+  'additionalItems', 'items', 'maxItems', 'minItems', 'uniqueItems',
+
+  'maxProperties', 'minProperties', 'required',
+  'additionalProperties', 'properties', 'patternProperties', 'dependencies',
+
+  'enum', 'type', 'allOf', 'anyOf', 'oneOf', 'not', 'definitions'
+];
+
+Schema.fromJSON = function (jsonSchema) {
+  if (isCircular(jsonSchema)) {
+    throw new TypeError('schema object is circular');
+  }
+
+  return new Schema({ '': jsonSchema }, '');
 };
+
+// -------------------- numeric --------------------
+Schema.prototype['$multipleOf'] = function $multipleOf () {
+  /* TODO: impl */
+};
+Schema.prototype['$maximum'] = function $maximum () {
+  /* TODO: impl */
+};
+Schema.prototype['$exclusiveMaximum'] = function $exclusiveMaximum () {
+  /* TODO: impl */
+};
+Schema.prototype['$minimum'] = function $minimum () {
+  /* TODO: impl */
+};
+Schema.prototype['$exclusiveMinimum'] = function $exclusiveMinimum () {
+  /* TODO: impl */
+};
+
+// -------------------- string ---------------------
+Schema.prototype['$maxLength'] = function $maxLength () {
+  /* TODO: impl */
+};
+Schema.prototype['$minLength'] = function $minLength () {
+  /* TODO: impl */
+};
+Schema.prototype['$pattern'] = function $pattern () {
+  /* TODO: impl */
+};
+
+// --------------------- array ---------------------
+Schema.prototype['$additionalItems'] = function $additionalItems () {
+  /* TODO: impl */
+};
+Schema.prototype['$items'] = function $items () {
+  /* TODO: impl */
+};
+Schema.prototype['$maxItems'] = function $maxItems () {
+  /* TODO: impl */
+};
+Schema.prototype['$minItems'] = function $minItems () {
+  /* TODO: impl */
+};
+Schema.prototype['$uniqueItems'] = function $uniqueItems () {
+  /* TODO: impl */
+};
+
+// -------------------- object ---------------------
+Schema.prototype['$maxProperties'] = function $maxProperties () {
+  /* TODO: impl */
+};
+Schema.prototype['$minProperties'] = function $minProperties () {
+  /* TODO: impl */
+};
+Schema.prototype['$required'] = function $required () {
+  /* TODO: impl */
+};
+Schema.prototype['$additionalProperties'] = function $additionalProperties () {
+  /* TODO: impl */
+};
+Schema.prototype['$properties'] = function $properties () {
+  /* TODO: impl */
+};
+Schema.prototype['$patternProperties'] = function $patternProperties () {
+  /* TODO: impl */
+};
+Schema.prototype['$dependencies'] = function $dependencies () {
+  /* TODO: impl */
+};
+
+// ---------------------- any ----------------------
+Schema.prototype['$enum'] = function $enum () {
+  /* TODO: impl */
+};
+Schema.prototype['$type'] = function $type () {
+  /* TODO: impl */
+};
+Schema.prototype['$allOf'] = function $allOf () {
+  /* TODO: impl */
+};
+Schema.prototype['$anyOf'] = function $anyOf () {
+  /* TODO: impl */
+};
+Schema.prototype['$oneOf'] = function $oneOf () {
+  /* TODO: impl */
+};
+Schema.prototype['$not'] = function $not () {
+  /* TODO: impl */
+};
+Schema.prototype['$definitions'] = function $definitions () {
+  /* TODO: impl */
+};
+
+/*
+Schema.premitiveTypes = (function () {
+  var numeric, string, array, object, any;
+
+  numeric = [
+    'multipleOf',
+    'maximum', 'exclusiveMaximum',
+    'minimum', 'exclusiveMinimum'
+  ];
+  string = [
+    'maxLength', 'minLength', 'pattern'
+  ];
+  array = [
+    'additionalItems', 'items', 'maxItems', 'minItems', 'uniqueItems'
+  ];
+  object = [
+    'maxProperties', 'minProperties',
+    'required', 'additionalProperties', 'properties', 'patternProperties',
+    'dependencies'
+  ];
+  any = [
+    'enum', 'type', 'allOf', 'anyOf', 'oneOf', 'not', 'definitions'
+  ];
+
+  return {
+    'object' :  object.concat(any),
+    'array'  :   array.concat(any),
+    'string' :  string.concat(any),
+    'number' : numeric.concat(any),
+    'integer': numeric.concat(any),
+    'boolean':                any ,
+    'null'   :                any
+  };
+})();
+*/
 
 Schema.prototype.validate = function validate (obj) {
   
@@ -46,4 +270,6 @@ if (!module.parent) {
   
   assert.equal(isCircular(circularObj), true);
   assert.equal(isCircular({}), false);
+
+  console.log(util.inspect(Schema.fromJSON(metaschemaJSON), {depth: null, colors:true}));
 }

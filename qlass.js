a4ca2d5f9cc015feacdba02ed5a8fc0fe37157f1
Hidekazu Kobayashi <hidekazu.kobayashi@gehirn.co.jp>
2014-09-30 02:40:29 +0900

    impl methods get hierarchy info

--- qlass.js
+++ qlass.js
@@ -1,78 +1,118 @@
-var Qlass = module.exports = {};
+'use strict';
 
-Qlass.$extend = function (statics, dynamics) {
-  statics.$proto = Object.create(this.$proto);
+var BaseQlass = {};
 
-  Object.keys(dynamics).forEach(function (key) {
-    Object.defineProperty(
-      statics.$proto, key,
-      Object.getOwnPropertyDescriptor(dynamics, key));
+BaseQlass.$extend = function (statics, dynamics) {
+  var Super = this,
+      Class = Object.create(Super);
+
+  Object.defineProperty(Class, '$proto', {
+    value: Object.create(Super.$proto)
   });
-
-  var Klass = Object.create(this);
+  Object.defineProperty(Class.$proto, '$class', {
+    value: Class
+  });
+  Object.defineProperty(Class, '$super', {
+    value: Super
+  });
 
   Object.keys(statics).forEach(function (key) {
     Object.defineProperty(
-      Klass, key,
+      Class, key,
       Object.getOwnPropertyDescriptor(statics, key));
   });
 
-  return Klass;
+  Object.keys(dynamics).forEach(function (key) {
+    Object.defineProperty(
+      Class.$proto, key,
+      Object.getOwnPropertyDescriptor(dynamics, key));
+  });
+
+  return Class;
 };
 
-Qlass.$proto = {};
+Object.defineProperties(BaseQlass, {
+  $proto: { value: {} },
+  $super: { value: BaseQlass }
+});
 
-Qlass = Qlass.$extend({
+var Qlass = BaseQlass.$extend({
   new: function (/* args */) {
-    var instance = Object.create(this.$proto),
-        ret = instance.ctor();
+    var Class, instance, ret;
+    Class = this;
+    instance = Object.create(Class.$proto);
+    ret = instance.ctor();
 
     return (ret === undefined) ? instance : ret;
+  },
+  isChildOf: function (Super) {
+    for (var Class = this; Class !== BaseQlass; Class = Class.$super) {
+      if (Class.$super === Super) {
+        return true;
+      }
+    }
+    return false;
   }
 }, {
-  ctor: function () { /* NOOP */}
+  ctor: function () { /* NOOP */ },
+  get $super() {
+    return Object.getPrototypeOf(this);
+  },
+  isInstanceOf: function (Class) {
+    return this.$class === Class || this.$class.isChildOf(Class);
+  }
 });
 
 module.exports = Qlass;
 
 if (!module.parent) {
-  var assert = require('assert');
+  (function () {
+    var assert = require('assert');
 
-  var Klass = Qlass.$extend({
-    // class member
-    foo: function () {
-      return 'foo';
-    },
-    bar: function () {
-      return 'bar';
-    }
-  }, {
-    // instance member
-    hoge: function () {
-      return 'hoge';
-    },
-    huga: function () {
-      return 'huga';
-    }
-  });
+    assert.ok(Qlass.isChildOf(BaseQlass));
+    
+    var Parent = Qlass.$extend({
+      // class member
+      foo: function () {
+        return 'foo';
+      },
+      bar: function () {
+        return 'bar';
+      }
+    }, {
+      // instance member
+      hoge: function () {
+        return 'hoge';
+      },
+      huga: function () {
+        return 'huga';
+      }
+    });
 
-  var Child = Klass.$extend({
-    // class member
-    baz: function () {
-      return 'baz';
-    }
-  }, {
-    // instance member
-    piyo: function () {
-      return 'piyo';
-    }
-  });
+    var Child = Parent.$extend({
+      // class member
+      baz: function () {
+        return 'baz';
+      }
+    }, {
+      // instance member
+      piyo: function () {
+        return 'piyo';
+      }
+    });
 
-  assert.equal(Klass.foo(), 'foo');
-  assert.equal(Child.foo(), 'foo');
-  assert.equal(Child.baz(), 'baz');
+    assert.equal(Parent.foo(), 'foo');
+    assert.equal(Child.foo(), 'foo');
+    assert.equal(Child.baz(), 'baz');
 
-  var i = Child.new();
-  assert.equal(i.hoge(), 'hoge');
-  assert.equal(i.piyo(), 'piyo');
+    var i = Child.new();
+    assert.ok(Child.isChildOf(Parent));
+    assert.ok(!Child.isChildOf(Child));
+
+    assert.ok(i.isInstanceOf(Child));
+    assert.ok(i.isInstanceOf(Parent));
+
+    assert.equal(i.hoge(), 'hoge');
+    assert.equal(i.piyo(), 'piyo');
+  })();
 }
